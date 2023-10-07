df39e13741c9c989df0f6040032cd49367e6c64a
Ian Hickey <iansemailaccount@gmailo.com>
2023-10-07 06:24:34 -0400

    add db types

--- scripts/dm.bat
+++ scripts/dm.bat
@@ -13,7 +13,7 @@ REM Collect inputs from the user
 set /p PROJECT_NAME="Enter project name (e.g., todo, my-todo-app): "
 set /p PACKAGE_NAME="Enter package name (e.g., org.ionatomics, org.acme, com.ed.ian): "
 set /p TEMPLATE_NAME="Enter name of the example (rest, entity, todo, etc.): "
-set /p DB_TYPE="Enter Database Type (mysql, mariadb, etc.) or press Enter to skip: "
+set /p DB_TYPE="Enter Database Type (mysql, mariadb, postgresql, oracle, h2, derby, mssql, db2) or press Enter to skip: "
 
 REM Execute the Java CLI
 java -jar dm.jar -n %PROJECT_NAME% -p %PACKAGE_NAME% -t %TEMPLATE_NAME% -d %DB_TYPE%
